9e34e29c8126d2a8446154dea06ca59d35fc02c9
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2026-02-15 19:00:16 +0100

    fix: removed unused import

--- src/main/java/fr/gabhas/FFA/FFA.java
+++ src/main/java/fr/gabhas/FFA/FFA.java
@@ -4,18 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.jruby.embed.ScriptingContainer;
 
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 public final class FFA extends JavaPlugin {
-
-    private ScriptingContainer rbContainer;
 
     @Override
     public void onEnable() {
         getLogger().info("Starting JRuby...");
 
-        rbContainer = new ScriptingContainer();
+        ScriptingContainer rbContainer = new ScriptingContainer();
 
         // Make the plugin accessible from Ruby as the `($)plugin` variable
         rbContainer.put("plugin", this);
