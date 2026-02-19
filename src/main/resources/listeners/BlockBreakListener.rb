a50edda82dd36c4da54977ecd72ec6e848eb7954
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2026-02-19 10:52:15 +0100

    fix: Preventing player in adventure/ survival mode from breaking block

--- src/main/resources/listeners/BlockBreakListener.rb
+++ src/main/resources/listeners/BlockBreakListener.rb
@@ -3,10 +3,7 @@
 java_import org.bukkit.event.Listener
 java_import org.bukkit.event.block.BlockBreakEvent
 java_import org.bukkit.plugin.EventExecutor
-
-java_import Java::net.kyori.adventure.text.Component
-java_import Java::net.kyori.adventure.text.format.NamedTextColor
-java_import Java::net.kyori.adventure.text.format.TextDecoration
+java_import org.bukkit.GameMode
 
 
 class BlockBreakListener
@@ -15,15 +12,9 @@ class BlockBreakListener
   def self.executor(plugin)
     EventExecutor.impl do |_method, _listener, event|
       if event.is_a?(BlockBreakEvent)
-        player = event.get_player
-        block = event.get_block
-        message = Component.text("#{player.get_name}")
-                    .color(NamedTextColor::GOLD)
-                    .append(Component.text(" broke ").color(NamedTextColor::GRAY))
-                    .append(Component.text("#{block.get_type} ").color(NamedTextColor::GOLD))
-                    .append(Component.text(" (x: #{block.get_x}, y: #{block.get_y}, z: #{block.get_z})").color(NamedTextColor::GRAY))
-
-        player.send_message(message)
+        if event.get_player.get_game_mode != GameMode::CREATIVE
+          event.set_cancelled(true)
+        end
       end
     end
   end
