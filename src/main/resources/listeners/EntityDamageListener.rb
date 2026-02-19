559b0483efae89762bd2c9173c65fcf5fb6e3766
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2026-02-19 16:21:47 +0100

    fix: Prevent player from taking damage when they are in spawn (not in arena, neither in combat)

--- src/main/resources/listeners/EntityDamageListener.rb
+++ src/main/resources/listeners/EntityDamageListener.rb
@@ -11,12 +11,12 @@ class EntityDamageListener
 
   def self.handle_entity_damage(event)
     # The logic is: a player falls in the arena (since the spawn is above)
-    # so the inArena state is updated on fall damage (and damage are cancelled)
-    if event.get_cause == EntityDamageEvent::DamageCause::FALL
-      if event.get_entity.is_a?(Player)
-        player = event.get_entity
-        player_y = player.get_y
+    # so the inArena state is updated on fall damage (and damage are canceled)
+    if event.get_entity.is_a?(Player)
+      player = event.get_entity
+      player_y = player.get_y
 
+      if event.get_cause == EntityDamageEvent::DamageCause::FALL
         # A player is considered in
         # - Arena: -50 <= Y <= -10
         # - Spawn: -9 <= Y <= 20
@@ -25,6 +25,18 @@ class EntityDamageListener
         elsif (-9 <= player_y) && (player_y <= 20)
           Score.set_player_score(player, Score::IN_ARENA, 0)
         end
+        event.set_cancelled(true)  # Cancel fall damages
+      end
+
+      # Cancel damage if player is not in combat/ arena (meaning they should be in the spawn)
+      if Score.get_player_score(player, Score::IN_ARENA) == 0 && Score.get_player_score(player, Score::IN_COMBAT) == 0
+        event.set_cancelled
+      end
+    end
+    if event.get_cause == EntityDamageEvent::DamageCause::FALL
+      if event.get_entity.is_a?(Player)
+        player = event.get_entity
+
         event.set_cancelled(true)
       end
     end
